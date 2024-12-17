f86e84b9aa531a8d52b6910c56d3b5ba28037e7d
Utsavi Patil <17bee076@nirmauni.ac.in>
2024-12-17 21:30:41 +0530

    Project Set up

---
+++ src/main/java/com/utsavi/spring_react_demo/SpringReactDemoApplication.java
@@ -0,0 +1,13 @@
+package com.utsavi.spring_react_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactDemoApplication.class, args);
+	}
+
+}
